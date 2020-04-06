df59d9eda977e302910480e8a4e292e7191f5383
Kyle Steinsvaag <stonebay.kyle@gmail.com>
2020-04-06 14:11:29 -0600

    Completes binary search algorithm

--- Search.java
+++ Search.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Search {
     public static void main(String[] args) {
@@ -7,8 +8,13 @@ public class Search {
         for (String str : data) {
             haystack.add(str);
         }
+        
+        Collections.sort(haystack); // sort haystack alphabetically
+        String needle = "blueberry";
 
-        System.out.println(search(haystack, "blueberry"));
+        System.out.println("Basic search: " + search(haystack, needle));
+        
+        System.out.println("Binary Search: " + binarySearch(haystack, needle));
     }
 
     public static int search(ArrayList<String> haystack, String needle) {
@@ -17,5 +23,25 @@ public class Search {
         }
 
         return -1;
+    }
+
+    public static int binarySearch(ArrayList<String> haystack, String needle) {
+        int low = 0, high = haystack.size()-1;
+
+        int current = (high+low)/2;
+        while (high-low > 2) {
+            current = (high+low)/2;
+
+            int compare = needle.compareTo(haystack.get(current));
+            if (compare == 0) return current;
+            
+            if (compare < 0) { // needle is before
+                high = current;
+            } else { // needle is after
+                low = current;
+            }
+        }
+
+        return haystack.get(current).equals(needle) ? current : -1; // return current if true, -1 if false
     }
 }
